1739d9cd2db0442cc72075a2e26e976f421e2e78
Rfiuza <rogerio.barbosa.fiuza@gmail.com>
2023-08-27 09:51:50 -0300

    v2

--- App.java-conta-banco/src/ContaTerminal.java
+++ App.java-conta-banco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Por favor, digite o número da conta: ");
+        int numero = scanner.next();
+        
+        System.out.println("Digite o número da Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o seu nome impresso no cartão: ");
+        int idade = scanner.nextInt();
+        
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        
+        
     }
 }
---
+++ App.java-conta-banco/src/ReceberDados.java
@@ -0,0 +1,10 @@
+public class ReceberDados {
+
+int numero = 1;
+string agenciaUser = 1;
+string nomeUser = 1;
+float saldo = 1; 
+
+
+
+}
